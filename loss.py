1099034a827028871ff79f3d32661c76183dbd6f
kebaek <ke.baek@berkeley.edu>
2021-04-27 14:44:22 -0400

    normalize U

--- loss.py
+++ loss.py
@@ -110,6 +110,7 @@ class VariationalMaximalCodingRateReduction(torch.nn.Module):
         matrix_loss = 0.
         A = net.module.A.weight
         U = net.module.U.weight
+        U = torch.nn.functional.normalize(U)
         for j in range(k):
             norm = torch.norm(W.matmul(Pi[j]).matmul(W.T) - U@torch.diag(A[:,j])@U.T)
             matrix_loss += norm**2
