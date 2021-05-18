fe75d28987b4a82f679a0fbf6c92b3aeb48df26e
kebaek <ke.baek@berkeley.edu>
2021-05-18 15:57:01 -0400

    variational 10 modification

--- train_variational10.py
+++ train_variational10.py
@@ -1,7 +1,6 @@
 import argparse
 import os
-#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-#os.environ["CUDA_VISIBLE_DEVICES"] = "3,4,5,6,7"
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -112,7 +111,7 @@ X_train, y_train = F.get_samples(trainset, args.samples)
 X_test, y_test = F.get_samples(testset, args.samples)
 X_train = X_train.to(device)
 X_test = X_test.to(device)
-Pi = label_to_membership(y_train).to(device)
+Pi = label_to_membership(y_train)
 
 mcr = MCR2Variational(args.eps, args.mu)
 true_mcr = MaximalCodingRateReduction(args.eps)
@@ -125,35 +124,35 @@ with torch.no_grad():
     Z_train = net(X_train)
     total_r = 0
     for j in range(n_class):
-        ZZT = Z_train.T @ Pi[:, j].diag() @ Z_train
-        U, S, _ = torch.linalg.svd(ZZT)
+        U, S, _ = torch.linalg.svd(Z_train.T @ Pi[:, j].diag() @ Z_train)
         r = 1
         error = float('inf')
+        norm = torch.norm(Z@Pi[:,j].diag()@Z.T)
         while True:
-            error = torch.norm(ZZT - U[:,:r]@torch.diag(S[:r])@U[:,:r].T)**2
-            if error <= args.tol:
+            error = torch.norm(Z@Pi[:,j].diag()@Z.T - U[:,:r]@torch.diag(r(S[:r]))@U[:,:r].T)
+            normalized_error = error/norm
+            if normalized_error <= args.tol:
                 break
             r += 1
-        print(r)
         total_r += r
         init_U.append(U[:,:r])
         init_A.append(S[:r])
 print('r: %d'%total_r)
-U = torch.cat(init_U, dim=1)
+U = torch.cat(init_Us, dim=1)
 A = torch.zeros((n_class, total_r))
 r = 0
 for i in range(n_class):
-    A[i] = torch.tensor([0]*r + init_A[i].tolist() + [0]*(total_r -r -init_A[i].shape[0]))
+    A[i] = torch.tensor([0]*total_r + init_A[i].tolist() + [0]*(total_r - init_A[i].shape[0]))
     r += init_A[i].shape[0]
 
 U = nn.Parameter(
     U,
     requires_grad=True
-    )
+    ).to(device)
 A = nn.Parameter(
     A,
     requires_grad=True
-    )
+    ).to(device)
 optimizer_net = optim.SGD(net.parameters(), lr=args.net_lr)
 # optimizer_net = optim.Adadelta(net.parameters(), lr=args.net_lr)
 optimizer_UA = optim.SGD([U, A], lr=args.param_lr)
@@ -177,11 +176,11 @@ for epoch in range(args.epochs):
 
     for step in range(20):
         optimizer_UA.zero_grad()
-        loss_obj, loss_R, loss_Rc, loss_reg_U = criterion_mcr2var(Z, Pi, A, U)
+        loss_obj, loss_R, loss_Rc, loss_reg_U = criterion_mcr2var(Z_n, Pi, A, U)
         loss_obj.backward()
         optimizer_UA.step()
 
-    #if epoch % 200 == 0:
+    # if epoch % 200 == 0:
         # with torch.no_grad():
         #     Z_test = net(X_test)
         #acc_train, acc_test = metrics_sup.nearsub(Z_train, y_train, Z_test, y_test, n_class, 1)
